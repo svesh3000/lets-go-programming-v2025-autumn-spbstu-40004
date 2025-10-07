2d26f622c9d247a60ba030389d98f5c56d0e688c
Sveshnikov Michail <russkij2006@mail.ru>
2025-10-07 03:04:08 +0300

    [TASK-1] Input processing

--- michail.sveshnikov/task-1/cmd/service/main.go
+++ michail.sveshnikov/task-1/cmd/service/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var operand1, operand2 int
+	_, err := fmt.Scan(&operand1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+	}
+	_, err = fmt.Scan(&operand2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+	}
+	var operator string
+	_, err = fmt.Scan(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+	}
+}
