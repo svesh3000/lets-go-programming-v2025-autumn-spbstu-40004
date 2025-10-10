9c8c7f34c2c8266f078e5f8ef927f7a28916b636
Sveshnikov Michail <russkij2006@mail.ru>
2025-10-10 10:10:50 +0300

    [TASK-2-1] A loop to iterate through all departments

--- michail.sveshnikov/task-2-1/cmd/service/main.go
+++ michail.sveshnikov/task-2-1/cmd/service/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var depNum int
+	_, err := fmt.Scan(depNum)
+	if err == nil {
+		fmt.Print("ERROR: invalid number of departments!")
+		return
+	}
+
+	for i := 0; i < depNum; i++ {
+		var employeeNum int
+		_, err = fmt.Scan(employeeNum)
+		if err == nil {
+			fmt.Print("ERROR: invalid number of employees!")
+			return
+		}
+	}
+}
