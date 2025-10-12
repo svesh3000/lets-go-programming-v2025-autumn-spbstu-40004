2ded3724c38783305143eabce3e60387bb441722
Sveshnikov Michail <russkij2006@mail.ru>
2025-10-12 18:07:16 +0300

    [TASK-2-2] Data reading

--- michail.sveshnikov/task-2-2/cmd/service/main.go
+++ michail.sveshnikov/task-2-2/cmd/service/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+)
+
+func main() {
+	var numDishes int
+
+	_, err := fmt.Scanln(&numDishes)
+	if err != nil || numDishes < 1 || numDishes > 10000 {
+		fmt.Println("ERROR: invalid number of dishes!")
+
+		return
+	}
+
+	for range numDishes {
+		var preferenceLevel int
+
+		_, err = fmt.Scan(&preferenceLevel)
+		if err != nil || preferenceLevel < -10000 || preferenceLevel > 10000 {
+			fmt.Println("ERROR: invalid preference level of the dish!")
+
+			return
+		}
+	}
+
+	var num int
+
+	_, err = fmt.Scanln(&num)
+	if err != nil || num < 1 || num > numDishes {
+		fmt.Println("ERROR: invalid number of the kth dish by preference!")
+
+		return
+	}
+}
