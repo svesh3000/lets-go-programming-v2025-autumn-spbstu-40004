88fe256defbabdb2c05997dd945288f099817d2a
Sveshnikov Michail <russkij2006@mail.ru>
2025-10-13 17:17:50 +0300

    [TASK-2-2] Using the pile to sort dishes by priority

--- michail.sveshnikov/task-2-2/cmd/service/main.go
+++ michail.sveshnikov/task-2-2/cmd/service/main.go
@@ -1,36 +1,55 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
+
+	"github.com/svesh3000/task-2-2/internal/maxheap"
 )
 
 func main() {
 	var numDishes int
 
-	_, err := fmt.Scanln(&numDishes)
+	_, err := fmt.Scan(&numDishes)
 	if err != nil || numDishes < 1 || numDishes > 10000 {
 		fmt.Println("ERROR: invalid number of dishes!")
 
 		return
 	}
 
-	for range numDishes {
-		var preferenceLevel int
+	buffet := &maxheap.MaxHeap{}
+	heap.Init(buffet)
 
-		_, err = fmt.Scan(&preferenceLevel)
-		if err != nil || preferenceLevel < -10000 || preferenceLevel > 10000 {
+	for range numDishes {
+		var preference int
+
+		_, err = fmt.Scan(&preference)
+		if err != nil || preference < -10000 || preference > 10000 {
 			fmt.Println("ERROR: invalid preference level of the dish!")
 
 			return
 		}
+
+		heap.Push(buffet, preference)
 	}
 
 	var num int
 
-	_, err = fmt.Scanln(&num)
+	_, err = fmt.Scan(&num)
 	if err != nil || num < 1 || num > numDishes {
 		fmt.Println("ERROR: invalid number of the kth dish by preference!")
 
 		return
+	}
+
+	for range num - 1 {
+		heap.Pop(buffet)
+	}
+
+	var dish int
+
+	dish, ok := heap.Pop(buffet).(int)
+	if ok {
+		fmt.Println(dish)
 	}
 }
