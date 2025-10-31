bbe7f11d6a3e2491f92906e33787bd099f0fd33e
Sveshnikov Michail <russkij2006@mail.ru>
2025-10-31 21:11:41 +0300

    [TASK-3] Step 1. Splitting into files

--- michail.sveshnikov/task-3/cmd/service/main.go
+++ michail.sveshnikov/task-3/cmd/service/main.go
@@ -6,13 +6,8 @@ import (
 	"os"
 	"path/filepath"
 
-	"gopkg.in/yaml.v3"
+	"github.com/svesh3000/task-3/internal/config"
 )
-
-type Config struct {
-	InputFile  string `yaml:"input-file"`
-	OutputFile string `yaml:"output-file"`
-}
 
 func main() {
 	configPath := flag.String("config", "", "path to config file (required)")
@@ -21,48 +16,14 @@ func main() {
 		panic("ERROR: Flag --config is required!")
 	}
 
-	config, err := loadConfig(*configPath)
+	cfg, err := config.LoadConfig(*configPath)
 	if err != nil {
 		panic(fmt.Sprintf("ERROR: Failed to load config: %v", err))
 	}
 
-	dir := filepath.Dir(config.OutputFile)
+	dir := filepath.Dir(cfg.OutputFile)
 	os.MkdirAll(dir, os.ModePerm)
 
-	fmt.Println(config.InputFile)
-	fmt.Println(config.OutputFile)
-}
-
-func loadConfig(configPath string) (*Config, error) {
-	_, err := os.Stat(configPath)
-	if os.IsNotExist(err) {
-		return nil, fmt.Errorf("config file does not exist: %s", configPath)
-	}
-
-	data, err := os.ReadFile(configPath)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read config file: %w", err)
-	}
-
-	var config Config
-
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		return nil, fmt.Errorf("cannot parse YAML config: %w", err)
-	}
-
-	if config.InputFile == "" {
-		return nil, fmt.Errorf("input-file field is required in config")
-	}
-
-	if config.OutputFile == "" {
-		return nil, fmt.Errorf("output-file field is required in config")
-	}
-
-	_, err = os.Stat(config.InputFile)
-	if os.IsNotExist(err) {
-		return nil, fmt.Errorf("input file does not exist: %s", config.InputFile)
-	}
-
-	return &config, nil
+	fmt.Println(cfg.InputFile)
+	fmt.Println(cfg.OutputFile)
 }
