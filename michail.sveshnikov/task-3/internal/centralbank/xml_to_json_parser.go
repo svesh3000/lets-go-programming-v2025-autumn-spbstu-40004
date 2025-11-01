b4717527e2a584db5cad1403a9bc37bb32215105
Sveshnikov Michail <russkij2006@mail.ru>
2025-11-01 05:51:30 +0300

    [TASK-3] Splitting ParseXMLFile into small functions:

--- michail.sveshnikov/task-3/internal/centralbank/xml_to_json_parser.go
+++ michail.sveshnikov/task-3/internal/centralbank/xml_to_json_parser.go
@@ -31,17 +31,25 @@ type (
 )
 
 func ParseXMLFile(filename string) ([]Currency, error) {
+	content, err := readAndPreprocessXML(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	tempValCursData, err := decodeXMLContent(content)
+	if err != nil {
+		return nil, err
+	}
+
+	return convertToCurrencies(tempValCursData)
+}
+
+func readAndPreprocessXML(filename string) ([]byte, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, fmt.Errorf("cannot open XML file: %w", err)
 	}
-
-	defer func() {
-		closeErr := file.Close()
-		if closeErr != nil {
-			_ = closeErr
-		}
-	}()
+	defer file.Close()
 
 	content, err := io.ReadAll(file)
 	if err != nil {
@@ -50,46 +58,63 @@ func ParseXMLFile(filename string) ([]Currency, error) {
 
 	content = bytes.ReplaceAll(content, []byte(","), []byte("."))
 
+	return content, nil
+}
+
+func decodeXMLContent(content []byte) (*tempValCurs, error) {
 	decoder := xml.NewDecoder(bytes.NewReader(content))
 	decoder.CharsetReader = charset.NewReaderLabel
-
 	var tempValCursData tempValCurs
 
-	err = decoder.Decode(&tempValCursData)
+	err := decoder.Decode(&tempValCursData)
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse XML: %w", err)
 	}
 
+	return &tempValCursData, nil
+}
+
+func convertToCurrencies(tempValCursData *tempValCurs) ([]Currency, error) {
 	currencies := make([]Currency, 0, len(tempValCursData.Valutes))
 
 	for _, tempValute := range tempValCursData.Valutes {
-		numCode := 0
-
-		if tempValute.NumCode != "" {
-			var err error
-
-			numCode, err = strconv.Atoi(tempValute.NumCode)
-			if err != nil {
-				return nil, fmt.Errorf("cannot parse NumCode for currency %s: %w", tempValute.CharCode, err)
-			}
-		}
-
-		charCode := tempValute.CharCode
-		if charCode == "" {
-			charCode = ""
-		}
-
-		value, err := strconv.ParseFloat(tempValute.Value, 64)
+		currency, err := convertValuteToCurrency(tempValute)
 		if err != nil {
-			return nil, fmt.Errorf("cannot parse Value for currency %s: %w", tempValute.CharCode, err)
+			return nil, err
 		}
 
-		currencies = append(currencies, Currency{
-			NumCode:  numCode,
-			CharCode: charCode,
-			Value:    value,
-		})
+		currencies = append(currencies, currency)
 	}
 
 	return currencies, nil
+}
+
+func convertValuteToCurrency(tempValute tempValute) (Currency, error) {
+	numCode, err := parseNumCode(tempValute.NumCode)
+	if err != nil {
+		return Currency{}, fmt.Errorf("cannot parse NumCode for currency %s: %w", tempValute.CharCode, err)
+	}
+
+	value, err := parseValue(tempValute.Value)
+	if err != nil {
+		return Currency{}, fmt.Errorf("cannot parse Value for currency %s: %w", tempValute.CharCode, err)
+	}
+
+	return Currency{
+		NumCode:  numCode,
+		CharCode: tempValute.CharCode,
+		Value:    value,
+	}, nil
+}
+
+func parseNumCode(numCodeStr string) (int, error) {
+	if numCodeStr == "" {
+		return 0, nil
+	}
+
+	return strconv.Atoi(numCodeStr)
+}
+
+func parseValue(valueStr string) (float64, error) {
+	return strconv.ParseFloat(valueStr, 64)
 }
