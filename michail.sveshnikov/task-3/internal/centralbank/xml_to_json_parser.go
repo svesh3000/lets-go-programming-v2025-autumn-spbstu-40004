5bca7e7b49335f2990af70d365a6ec1a2c76725b
Sveshnikov Michail <russkij2006@mail.ru>
2025-12-05 14:05:06 +0300

    [TASK-3] Fix lint errors with tags

--- michail.sveshnikov/task-3/internal/centralbank/xml_to_json_parser.go
+++ michail.sveshnikov/task-3/internal/centralbank/xml_to_json_parser.go
@@ -15,9 +15,9 @@ type FloatComma float64
 
 type (
 	Currency struct {
-		NumCode  int        `xml:"NumCode" json:"num_code"`
-		CharCode string     `xml:"CharCode" json:"char_code"`
-		Value    FloatComma `xml:"Value" json:"value"`
+		NumCode  int        `json:"num_code" xml:"NumCode"`
+		CharCode string     `json:"char_code" xml:"CharCode"`
+		Value    FloatComma `json:"value" xml:"Value"`
 	}
 
 	tempValCurs struct {
@@ -78,5 +78,6 @@ func (fc *FloatComma) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error
 	}
 
 	*fc = FloatComma(val)
+
 	return nil
 }
