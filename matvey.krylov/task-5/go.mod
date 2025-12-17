760c76c4d8ce4e39c46e7ff421389eb043daa6dd
mkryloff <112196689+mkryloff@users.noreply.github.com>
2025-12-17 15:08:10 +0300

    [TASK-5] (#119)
    * [TASK-5] Initial commit with implemented conveyer
    
    * [TASK-5] handlers probably done after test
    
    * [TASK-5] lint fixes
    
    * [TASK-5] fixes

---
+++ matvey.krylov/task-5/go.mod
@@ -0,0 +1,5 @@
+module github.com/mkryloff/task-5
+
+go 1.22.0
+
+require golang.org/x/sync v0.11.0
---
+++ matvey.krylov/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,103 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var ErrNoDecorator = errors.New("can't be decorated")
+
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+	for {
+		select {
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, "no decorator") {
+				return ErrNoDecorator
+			}
+
+			if !strings.HasPrefix(data, "decorated: ") {
+				data = "decorated: " + data
+			}
+
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	if len(outputs) == 0 {
+		return nil
+	}
+
+	var index int
+
+	for {
+		select {
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			select {
+			case outputs[index] <- data:
+			case <-ctx.Done():
+				return nil
+			}
+
+			index = (index + 1) % len(outputs)
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	if len(inputs) == 0 {
+		return nil
+	}
+
+	var wGroup sync.WaitGroup
+
+	wGroup.Add(len(inputs))
+
+	for iIndex := range inputs {
+		go func(idx int) {
+			defer wGroup.Done()
+
+			for {
+				select {
+				case data, ok := <-inputs[idx]:
+					if !ok {
+						return
+					}
+
+					if !strings.Contains(data, "no multiplexer") {
+						select {
+						case output <- data:
+						case <-ctx.Done():
+							return
+						}
+					}
+				case <-ctx.Done():
+					return
+				}
+			}
+		}(iIndex)
+	}
+
+	wGroup.Wait()
+
+	return nil
+}
