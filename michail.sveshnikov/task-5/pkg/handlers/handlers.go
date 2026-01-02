ddd8a2ccec6b2f9514079f9484fc2e09e51dfff2
Sveshnikov Michail <russkij2006@mail.ru>
2026-01-02 13:43:08 +0300

    [TASK-5] PrefixDecorator

--- michail.sveshnikov/task-5/pkg/handlers/handlers.go
+++ michail.sveshnikov/task-5/pkg/handlers/handlers.go
@@ -1,1 +1,44 @@
 package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+)
+
+var ErrNoDecoration = errors.New("can't be decorated")
+
+const (
+	noDecoratorMarker = "no decorator"
+	prefDecorated     = "decorated: "
+)
+
+func PrefixDecorator(ctx context.Context, input <-chan string, output chan<- string) error {
+	defer close(output)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, noDecoratorMarker) {
+				return ErrNoDecoration
+			}
+
+			processed := data
+			if !strings.HasPrefix(data, prefDecorated) {
+				processed = prefDecorated + data
+			}
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case output <- processed:
+			}
+		}
+	}
+}
