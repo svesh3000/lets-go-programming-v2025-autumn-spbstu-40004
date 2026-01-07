741b8a97418ee7f4e967cb86c7262360c07c916c
Sveshnikov Michail <russkij2006@mail.ru>
2026-01-07 19:46:00 +0300

    [TASK-6] Add simple test for GetAddresses

--- michail.sveshnikov/task-6/internal/wifi/wifi_test.go
+++ michail.sveshnikov/task-6/internal/wifi/wifi_test.go
@@ -1,13 +1,39 @@
 package wifi_test
 
 import (
+	"net"
 	"testing"
 
+	wifi "github.com/mdlayher/wifi"
 	"github.com/stretchr/testify/require"
+	mywifi "github.com/svesh3000/task-6/internal/wifi"
 )
 
-func TestWifi(t *testing.T) {
-	t.Run("placeholder", func(t *testing.T) {
-		require.True(t, true)
-	})
+func parseMAC(t *testing.T, macStr string) net.HardwareAddr {
+	t.Helper()
+
+	hwAddr, err := net.ParseMAC(macStr)
+	require.NoError(t, err)
+
+	return hwAddr
+}
+
+func TestGetAddresses(t *testing.T) {
+	mockWifi := NewWiFiHandle(t)
+
+	iface := &wifi.Interface{
+		Name:         "one",
+		HardwareAddr: parseMAC(t, "00:11:22:33:44:55"),
+	}
+	mockWifi.On("Interfaces").Return([]*wifi.Interface{iface}, nil)
+
+	service := mywifi.New(mockWifi)
+
+	addrs, err := service.GetAddresses()
+
+	require.NoError(t, err)
+	require.Len(t, addrs, 1)
+	require.Equal(t, parseMAC(t, "00:11:22:33:44:55"), addrs[0])
+
+	mockWifi.AssertExpectations(t)
 }
