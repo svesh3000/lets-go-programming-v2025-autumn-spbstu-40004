a5490aa3b9b5aeef1918fd4ce36a6eabb1f7d1e9
Sveshnikov Michail <russkij2006@mail.ru>
2026-01-07 15:31:34 +0300

    [TASK-6] Added tableTest and made test more complex, changed structure of tests

--- michail.sveshnikov/task-6/internal/db/db_test.go
+++ michail.sveshnikov/task-6/internal/db/db_test.go
@@ -8,28 +8,41 @@ import (
 	"github.com/svesh3000/task-6/internal/db"
 )
 
-func TestGetNamesSuccess(t *testing.T) {
-	mockDB, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("unexpected error when creating mock db: %v", err)
+func getMockDBRows(t *testing.T, names []string) *sqlmock.Rows {
+	t.Helper()
+
+	rows := sqlmock.NewRows([]string{"name"})
+	for _, name := range names {
+		rows = rows.AddRow(name)
 	}
-	defer mockDB.Close()
 
-	dbService := db.New(mockDB)
+	return rows
+}
 
-	expectedNames := []string{"One", "Two"}
+func TestGetNames(t *testing.T) {
+	t.Run("success with data", func(t *testing.T) {
+		mockDB, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer mockDB.Close()
 
-	mock.ExpectQuery("SELECT name FROM users").
-		WillReturnRows(
-			sqlmock.NewRows([]string{"numbers"}).
-				AddRow("One").
-				AddRow("Two"),
-		)
+		dbService := db.New(mockDB)
 
-	names, err := dbService.GetNames()
+		testTable := [][]string{
+			{"one", "two"},
+			{"one", "two", "three"},
+			{"one", ""},
+			{"", ""},
+		}
 
-	require.NoError(t, err)
-	require.Equal(t, expectedNames, names)
+		for _, row := range testTable {
+			mock.ExpectQuery("SELECT name FROM users").
+				WillReturnRows(getMockDBRows(t, row))
 
-	require.NoError(t, mock.ExpectationsWereMet())
+			names, err := dbService.GetNames()
+
+			require.NoError(t, err)
+			require.Equal(t, row, names)
+			require.NoError(t, mock.ExpectationsWereMet())
+		}
+	})
 }
