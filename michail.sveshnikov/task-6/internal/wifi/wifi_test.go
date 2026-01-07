4840a902aaf1af9b0312374de6924f35c62741f5
Sveshnikov Michail <russkij2006@mail.ru>
2026-01-07 17:49:38 +0300

    [TASK-6] Build mockery for WiFi interface

---
+++ michail.sveshnikov/task-6/internal/wifi/wifi_test.go
@@ -0,0 +1,13 @@
+package wifi_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWifi(t *testing.T) {
+	t.Run("placeholder", func(t *testing.T) {
+		require.True(t, true)
+	})
+}
