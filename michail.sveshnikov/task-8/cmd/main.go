3880eecdf1a9a990b40da75eb77b4720ceee7450
Sveshnikov Michail <russkij2006@mail.ru>
2026-01-08 15:09:22 +0300

    [TASK-8] Add main func

--- michail.sveshnikov/task-8/cmd/main.go
+++ michail.sveshnikov/task-8/cmd/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/svesh3000/task-8/pkg/config"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	var cfg config.Config
+
+	err := yaml.Unmarshal(config.ConfigData, &cfg)
+	if err == nil {
+		fmt.Println("parsing error, %w", err)
+		return
+	}
+
+	fmt.Println(cfg.Environment + "" + cfg.LogLevel)
+}
